79dcc72ec49a1f9105ce0994e9ba9a07aa96c71e
= <=>
2017-04-26 20:28:51 +0300

    br

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,12 +21,12 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'deploy',
-      host : '207.154.197.243',
+      user : 'root',
+      host : '46.101.211.213',
       ref  : 'origin/master',
       repo : 'https://github.com/pavelko1608/loginapp.git',
-      path : '/home/deploy/loginapp',
-      'post-deploy' : 'nvm install && npm install && /home/deploy/.nvm/versions/node/v6.10.2/bin/pm2 reload ecosystem.config.js --env production'
+      path : '/home/root/loginapp',
+      'post-deploy' : 'npm install && /home/deploy/.nvm/versions/node/v6.10.2/bin/pm2 reload ecosystem.config.js --env production'
     }
   }
 };
